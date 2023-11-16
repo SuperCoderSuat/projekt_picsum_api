d984060c5c1f777d01721c1d2420ed25f8ed8033
SuperCoderSuat <supercodesuat@gmail.com>
2023-11-16 15:09:27 +0100

    update include error handler

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,35 +4,41 @@
 
 fetch('https://picsum.photos/v2/list')
     .then(response => {
-
-        return response.json();
-
+    if (!response.ok) {
+        throw new Error('Fehler beim Abrufen der Daten');
+    }
+    return response.json();
     })
     .then(userArr => {
+    console.log(userArr);
 
-        console.log(userArr);
+    const imgContainer = document.body.querySelector('#img_container');
+    imgContainer.style.padding = '20px';
 
-        const imgContainer = document.body.querySelector('#img_container');
-        imgContainer.style.padding = '20px';
+    userArr.forEach(singleElement => {
+        const figure = document.createElement('figure');
+        figure.classList.add('img_item');
 
-        userArr.forEach(singleElement => {
+        const img = document.createElement('img');
+        img.src = singleElement.download_url;
+        img.style.width = '100%';
 
-            const figure = document.createElement('figure');
-            figure.classList.add('img_item');
-            const img = document.createElement('img');
-            img.setAttribute('src', singleElement.download_url)
-            img.style.width = '100%';
-            const figcaption = document.createElement('figcaption');
-            figcaption.textContent = singleElement.author;
-            const btn = document.createElement('button');
-            btn.textContent = 'show more';
+        const figcaption = document.createElement('figcaption');
+        figcaption.textContent = singleElement.author;
 
-            btn.addEventListener('click', () => {
-                window.open(singleElement.url, '_blank'); // Öffne die URL in einem neuen Tab/Fenster
-            });
+        const btn = document.createElement('button');
+        btn.textContent = 'Mehr anzeigen';
 
-            imgContainer.appendChild(figure);
-            figure.append(img, figcaption, btn);
-
+        btn.addEventListener('click', () => {
+        if (singleElement.url) {
+            window.open(singleElement.url, '_blank');
+        } else {
+            alert('URL nicht verfügbar');
+        }
         });
+
+        imgContainer.appendChild(figure);
+        figure.append(img, figcaption, btn);
     });
+    })
+    .catch(error => console.error('Fehler:', error.message));
